6c59770df0bfc8e9d9ce9990028677b61a30fe0a
mimi <hongmikyeong11@gmail.com>
2022-11-14 23:15:34 +0900

    chore : cra를 통한 리액트 개발 환경 생성

---
+++ pagination/src/App.js
@@ -0,0 +1,16 @@
+import Item from "./components/item";
+import data from "./data/data.json";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        {data.map((value, index) => (
+          <Item key={index} data={value.title} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
